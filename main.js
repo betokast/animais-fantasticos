e0ee381161423a62f09b9e5eff985398a7797281
robertoCastro <bkastro@gmail.com>
2024-02-08 23:05:00 -0300

    Inclusão do script para a animação do scroll

--- main.js
+++ main.js
@@ -40,27 +40,45 @@ function initAccordion() {
 }
 initAccordion();
 
-function initSmoothScroll(){
+function initSmoothScroll() {
   const linksInternos = document.querySelectorAll('.js-menu a[href^="#"');
   function scrollToSection(event) {
     event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
+    const href = event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
     //Forma otimizada de fazer o scroll suave
     section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
+      behavior: "smooth",
+      block: "start",
     });
-
     //Forma alternativa de fazer o scroll suave
     // const topo = section.offsetTop;
     // window.scrollTo({
-      //   top: topo,
-      //   behavior: 'smooth'
-      // })
+    //   top: topo,
+    //   behavior: 'smooth'
+    // })
   }
   linksInternos.forEach((link) => {
     link.addEventListener("click", scrollToSection);
   });
 }
 initSmoothScroll();
+
+function initScrollAnimation(){
+  const sections = document.querySelectorAll(".js-scroll");
+  if(sections.length){
+    const halfWindow = innerHeight * 0.6;
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - halfWindow) < 0;
+        if(isSectionVisible) {
+          section.classList.add('ativo');
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initScrollAnimation();
